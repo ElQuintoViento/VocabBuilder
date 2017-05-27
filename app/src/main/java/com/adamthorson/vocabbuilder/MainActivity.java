18160307d7a223d3c6ec971a7247d80ba9aa37d5
adthor <adam@thalchemy.com>
2017-05-27 18:03:55 -0500

    setup DrawerLayout & Navigation view using http://www.ssaurel.com/blog/implement-a-navigation-drawer-with-a-toolbar-on-android-m/ along with implementing ActionBarDrawerToggle

--- app/src/main/java/com/adamthorson/vocabbuilder/MainActivity.java
+++ app/src/main/java/com/adamthorson/vocabbuilder/MainActivity.java
@@ -3,7 +3,10 @@ package com.adamthorson.vocabbuilder;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -34,64 +37,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerOptions = getResources().getStringArray(R.array.drawer_array);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        listViewDrawer = (ListView) findViewById(R.id.list_view_drawer);
-
-        // set a custom shadow that overlays the main content when the drawer opens
-        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-
-        // Set adapter
-        Log.d(TAG, Arrays.toString(drawerOptions));
-        listViewDrawer.setAdapter(
-                new ArrayAdapter<String>(
-                        this,
-                        R.layout.drawer_list_item,
-                        drawerOptions
-                )
-        );
-
-        // enable ActionBar app icon to behave as action to toggle nav drawer
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Title");
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(false);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer_2);
-        getSupportActionBar().setDisplayShowCustomEnabled(true); // enable overriding the default toolbar layout
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        // ActionBarDrawerToggle ties together the the proper interactions
-        // between the sliding drawer and the action bar app icon
-        drawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                drawerLayout,         /* DrawerLayout object */
-                //R.drawable.ic_drawer_2,  /* nav drawer image to replace 'Up' caret */
-                //toolbar,
-                R.string.drawer_open,  /* "open drawer" description for accessibility */
-                R.string.drawer_close  /* "close drawer" description for accessibility */
-        ) {
-            public void onDrawerClosed(View view) {
-                //getSupportActionBar().setTitle("");
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                //getSupportActionBar().setTitle("");
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        drawerLayout.setDrawerListener(drawerToggle);
-
-
+        configureNavigationDrawer();
+        configureToolbar();
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = drawerLayout.isDrawerOpen(listViewDrawer);
-        //menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -109,15 +60,80 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
+        Log.d(TAG, "hit");
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer_2);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void configureNavigationDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                Fragment f = null;
+                int itemId = menuItem.getItemId();
+
+                if (itemId == R.id.action_drawer_main) {
+                    f = new MainFragment();
+                }
+                else if (itemId == R.id.action_drawer_settings) {
+                    f = new SettingsFragment();
+                }
+
+                if (f != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame, f);
+                    transaction.commit();
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                //getActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //getActionBar().setTitle(mDrawerTitle);
+            }
+        };
+        // Set the drawer toggle as the DrawerListener
+        drawerLayout.setDrawerListener(drawerToggle);
     }
 }
