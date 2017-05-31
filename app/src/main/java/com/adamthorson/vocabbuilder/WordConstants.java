2c8026b26eec517dfa83d42d4fe4297cba3ad12c
adthor <adam@thalchemy.com>
2017-05-31 16:27:24 -0500

    used to store different constants (e.g. intent request strings)

---
+++ app/src/main/java/com/adamthorson/vocabbuilder/WordConstants.java
@@ -0,0 +1,13 @@
+package com.adamthorson.vocabbuilder;
+
+/**
+ * Created by tor on 5/29/17.
+ */
+public final class WordConstants {
+    //
+    public static final String REQUEST_STRING_WORD = (
+            String.format("%s.REQUEST_STRING_WORD", BuildConfig.APPLICATION_ID));
+    public static final String REQUEST_OBJ_WORD = (
+            String.format("%s.REQUEST_OBJ_WORD", BuildConfig.APPLICATION_ID));
+    public static final int REQUEST_CODE_WORD = 1;
+}
